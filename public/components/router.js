0a83270e14e04d1705e99b683cb4021de7071a3b
John Rodney <johnrodney@Johns-iMac-3.local>
2015-04-10 14:36:16 -0500

    working on getting hero transistions to get past shadow route level problems

--- public/components/router.js
+++ public/components/router.js
@@ -1,13 +1,33 @@
 var router = Router({
   autoHash: true,
   '/user': {
+    on: function(){
+      console.log('routing to user');
+    },
     '/:id': {
       on: function(id){
         console.log(id);
       }
     }
   },
+  '/directory': {
+    on: function(){
+      console.log('routing to directory');
+    }
+  },
   notFound: function(){
     console.log('not Found');
   }
 });
+
+var fakeDB = (function(){
+    var temp = [];
+    for(var x = 0; x < 300; x++){
+      temp[x] = {
+        name: "Willy Wonka",
+        company: "Chocolate Factory",
+      }
+    }
+    return temp;
+  })();
+var NAME = 'willy wonka';
